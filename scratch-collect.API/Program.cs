33a06dec756d6da9628c4b037ab2aa7894c32658
kekebeen <bbabic081@gmail.com>
2021-07-11 22:53:29 +0200

    feat(startup): add startup seed initializer

--- scratch-collect.API/Program.cs
+++ scratch-collect.API/Program.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using scratch_collect.API.Database;
 
 namespace scratch_collect.API
 {
@@ -13,7 +14,15 @@ namespace scratch_collect.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            // TODO: check this one out ?
+            using (var scope = host.Services.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetRequiredService<ScratchCollectContext>();
+                Data.Seed(service);
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ scratch-collect.API/SetupService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using scratch_collect.API.Database;
+
+namespace scratch_collect.API
+{
+    public class SetupService
+    {
+        public static void Init(ScratchCollectContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
