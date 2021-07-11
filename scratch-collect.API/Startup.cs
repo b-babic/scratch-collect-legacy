71f1c1d50c5ae46a34c6e4684ec7b4365b7b83a7
kekebeen <bbabic081@gmail.com>
2021-07-11 22:55:56 +0200

    fix(startup): gracefully handle json serialize out of bounds

--- scratch-collect.API/Startup.cs
+++ scratch-collect.API/Startup.cs
@@ -20,6 +20,11 @@ namespace scratch_collect.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddControllers(x => x.Filters.Add(new ErrorFilter()))
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
 
 
             services.AddDbContext<ScratchCollectContext>(options =>
