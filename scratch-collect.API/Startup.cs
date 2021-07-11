598ca22c727925d935102cce9a6afbd82e0673e4
kekebeen <bbabic081@gmail.com>
2021-07-11 22:56:56 +0200

    feat(startup): add swagger definition

--- scratch-collect.API/Startup.cs
+++ scratch-collect.API/Startup.cs
@@ -28,11 +28,44 @@ namespace scratch_collect.API
 
             services.AddRouting(options => options.LowercaseUrls = true);
 
-            services.AddDbContext<ScratchCollectContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("collect")));
-
             // register swagger document
-            services.AddSwaggerDocument();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Scratch & Collect API",
+                    Version = "v1"
+                });
+
+                // add auth
+                c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "basic",
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "basic"
+                            }
+                        },
+                        System.Array.Empty<string>()
+                    }
+                });
+            });
+                
+                services.AddDbContext<ScratchCollectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("collect")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +77,11 @@ namespace scratch_collect.API
             }
 
             // add swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Scratch & Collect API");
+            });
 
             app.UseHttpsRedirection();
             app.UseRouting();
