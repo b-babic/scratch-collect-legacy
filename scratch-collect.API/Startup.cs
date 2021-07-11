31e177a53d52808b1e822d7290b7b92501d69b66
kekebeen <bbabic081@gmail.com>
2021-07-11 22:52:49 +0200

    fix(refactor): cleanup

--- scratch-collect.API/Startup.cs
+++ scratch-collect.API/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using scratch_collect.API.Database;
 
 namespace scratch_collect.API
@@ -28,7 +21,6 @@ namespace scratch_collect.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
 
             services.AddDbContext<ScratchCollectContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("collect")));
@@ -45,14 +37,9 @@ namespace scratch_collect.API
                 app.UseDeveloperExceptionPage();
             }
 
-            // third party
-
             // add swagger
-            app.UseOpenApi();
-            app.UseSwaggerUi3();
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
