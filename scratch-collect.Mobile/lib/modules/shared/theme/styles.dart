3dbb272683c941a20f9adc021892e67282967552
Benjamin Babic <bbabic081@gmail.com>
2022-06-20 15:37:15 +0200

    feat(mobile): add base heading style token

---
+++ scratch-collect.Mobile/lib/modules/shared/theme/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+import 'package:scratch_collect/modules/shared/theme/colors.dart';
+import 'package:scratch_collect/modules/shared/theme/utils.dart';
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: textColor,
+  height: 1.5,
+);
