61ef8b6b17999354c49821b3bc8f89702d11581d
Benjamin Babic <bbabic081@gmail.com>
2022-07-05 10:40:08 +0200

    feat(mobile): re-layout all screen loaders

--- scratch-collect.Mobile/lib/modules/home/widgets/offer_details_body_loading.dart
+++ scratch-collect.Mobile/lib/modules/home/widgets/offer_details_body_loading.dart
@@ -12,34 +12,55 @@ class OfferDetailsBodyLoading extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
         child: Column(
           children: <Widget>[
-            Column(
-              children: [
-                SkeletonAvatar(
-                  style: SkeletonAvatarStyle(
-                    height: getProportionateScreenHeight(250),
-                    width: getProportionateScreenWidth(250),
-                    shape: BoxShape.circle,
+            SkeletonLine(
+              style: SkeletonLineStyle(
+                height: getProportionateScreenHeight(250),
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            SkeletonLine(
+              style: SkeletonLineStyle(
+                  height: getProportionateScreenHeight(30),
+                  width: getProportionateScreenHeight(100)),
+            ),
+            SizedBox(height: getProportionateScreenHeight(20)),
+            SkeletonLine(
+              style: SkeletonLineStyle(
+                height: getProportionateScreenHeight(30),
+                width: getProportionateScreenHeight(160),
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(45)),
+            SkeletonLine(
+              style: SkeletonLineStyle(
+                height: getProportionateScreenHeight(60),
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(60)),
+            SkeletonLine(
+              style: SkeletonLineStyle(
+                height: getProportionateScreenHeight(30),
+                width: getProportionateScreenWidth(100),
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(35)),
+            Row(
+              children: <Widget>[
+                SkeletonLine(
+                  style: SkeletonLineStyle(
+                    width: getProportionateScreenWidth(100),
+                    height: getProportionateScreenHeight(100),
                   ),
-                )
+                ),
+                SizedBox(width: getProportionateScreenWidth(35)),
+                SkeletonLine(
+                  style: SkeletonLineStyle(
+                    width: getProportionateScreenWidth(100),
+                    height: getProportionateScreenHeight(100),
+                  ),
+                ),
               ],
             ),
-            const SizedBox(height: 30),
-            SkeletonLine(
-                style: SkeletonLineStyle(
-                    height: getProportionateScreenHeight(40))),
-            const SizedBox(height: 30),
-            SkeletonLine(
-                style: SkeletonLineStyle(
-                    height: getProportionateScreenHeight(60))),
-            const SizedBox(height: 20),
-            SkeletonLine(
-                style: SkeletonLineStyle(
-                    height: getProportionateScreenHeight(60))),
-            const SizedBox(height: 20),
-            SkeletonLine(
-                style: SkeletonLineStyle(
-                    height: getProportionateScreenHeight(60))),
-            const SizedBox(height: 20),
           ],
         ));
   }
