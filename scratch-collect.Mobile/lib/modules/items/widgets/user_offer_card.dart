7dd9e5f5fe1e877ce4a8d7eb4837dcc60a582efe
Benjamin Babic <bbabic081@gmail.com>
2022-08-28 19:56:28 +0200

    feat(mobile): introduce already rated info

--- scratch-collect.Mobile/lib/modules/items/widgets/user_offer_card.dart
+++ scratch-collect.Mobile/lib/modules/items/widgets/user_offer_card.dart
@@ -1,3 +1,4 @@
+import 'package:IB210370/modules/home/widgets/rating_info.dart';
 import 'package:flutter/material.dart';
 import 'package:IB210370/modules/home/widgets/gradient_background.dart';
 import 'package:IB210370/modules/items/models/play_screen_arguments.model.dart';
@@ -24,59 +25,61 @@ class UserOfferCard extends StatelessWidget {
       children: <Widget>[
         SizedBox(
           width: SizeConfig.screenWidth,
-          child: GestureDetector(
-            onTap: () => Navigator.pushNamed(
-              context,
-              PlayScreen.routeName,
-              arguments: PlayScreenArguments(
-                userOffer.id,
-                userOffer.offer?.id,
-                userOffer.offer?.requiredPrice,
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Card(
-                  elevation: 3,
-                  child: Padding(
-                    padding: EdgeInsets.all(
-                      getProportionateScreenWidth(20),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        GradientBackground(
-                          category: userOffer.offer?.category,
-                          tag: keyPrefix.isNotEmpty
-                              ? "$keyPrefix-${userOffer.id.toString()}"
-                              : userOffer.id.toString(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                elevation: 3,
+                child: Padding(
+                  padding: EdgeInsets.all(
+                    getProportionateScreenWidth(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      GradientBackground(
+                        category: userOffer.offer?.category,
+                        tag: keyPrefix.isNotEmpty
+                            ? "$keyPrefix-${userOffer.id.toString()}"
+                            : userOffer.id.toString(),
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(15),
+                      ),
+                      Text(
+                        userOffer.offer?.title ?? "",
+                        style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: getProportionateScreenWidth(16),
                         ),
-                        SizedBox(
-                          height: getProportionateScreenHeight(15),
-                        ),
-                        Text(
-                          userOffer.offer?.title ?? "",
-                          style: TextStyle(
-                            color: textColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: getProportionateScreenWidth(16),
-                          ),
-                          maxLines: 2,
-                        ),
-                        SizedBox(
-                          height: getProportionateScreenHeight(30),
-                        ),
-                        const Button(
-                          text: "Scratch & Collect",
-                        ),
+                        maxLines: 2,
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(30),
+                      ),
+                      if (userOffer.alreadyRated ?? false) ...[
+                        RatingInfo(rating: userOffer.averageRating ?? 0.0),
                       ],
-                    ),
+                      Button(
+                        text: "Scratch & Collect",
+                        press: () => Navigator.pushNamed(
+                          context,
+                          PlayScreen.routeName,
+                          arguments: PlayScreenArguments(
+                            userOffer.id,
+                            userOffer.offer?.id,
+                            userOffer.offer?.requiredPrice,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: getProportionateScreenHeight(10)),
-              ],
-            ),
+              ),
+              SizedBox(height: getProportionateScreenHeight(10)),
+            ],
           ),
         ),
         SizedBox(
