54d1d719dd5f8ccc5aee0851e8f6d9f8f43ddf00
Benjamin Babic <bbabic081@gmail.com>
2023-07-12 16:31:38 +0200

    feat: align packages between osx and windows

--- scratch-collect.Mobile/lib/modules/shared/constants.dart
+++ scratch-collect.Mobile/lib/modules/shared/constants.dart
@@ -1,5 +1,5 @@
-// const url = 'https://10.0.2.2:5001/api'; // Windows localhost
+const url = 'https://10.0.2.2:5001/api'; // Windows localhost
 // const url = 'http://10.0.2.2:5010/api'; // Windows docker
 
-const url = 'https://127.0.0.1:5001/api'; // OSX local
+// const url = 'https://127.0.0.1:5001/api'; // OSX local
 // const url = 'http://127.0.0.1:5010/api'; // OSX docker
