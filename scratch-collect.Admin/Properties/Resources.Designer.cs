e121b7f8ab66c94ceef3f688cc468e5be504c80c
b-babic <11808448+b-babic@users.noreply.github.com>
2024-06-18 21:51:48 +0200

    feat(admin): set winforms to become legacy project

--- scratch-collect.Admin/Properties/Resources.Designer.cs
+++ scratch-collect.Admin/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace scratch_collect.Admin.Properties {
+namespace scratch_collect.AdminLegacy.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace scratch_collect.Admin.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("scratch_collect.Admin.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("scratch_collect.AdminLegacy.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
